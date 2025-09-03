0cb9315fef4e099e01be3c8beb57647f57db6391
Chris Lee <git@chrislee.kr>
2025-09-03 20:38:16 +1000

    fix: coredns issue

--- stage2/kubernetes/coredns-custom.tf
+++ stage2/kubernetes/coredns-custom.tf
@@ -1,75 +1,75 @@
 # My internet provider does not support hairpin NAT, so I need to use the custom coredns config to avoid it.
 
-# Configure the coredns custom configmap to avoid hairpin NAT issue.
-resource "kubernetes_config_map" "coredns_custom" {
-  metadata {
-    name      = "coredns-custom"
-    namespace = "kube-system"
-  }
+# # Configure the coredns custom configmap to avoid hairpin NAT issue.
+# resource "kubernetes_config_map" "coredns_custom" {
+#   metadata {
+#     name      = "coredns-custom"
+#     namespace = "kube-system"
+#   }
 
-  data = {
-    "default.server"   = <<EOF
-${trim(var.kubernetes_override_domains, "\"")} {
-    hosts {
-          ${var.kubernetes_override_ip} ${trim(var.kubernetes_override_domains, "\"")}
-          fallthrough
-    }
-}
-EOF
-    "default.override" = ""
-  }
-}
+#   data = {
+#     "default.server"   = <<EOF
+# ${trim(var.kubernetes_override_domains, "\"")} {
+#     hosts {
+#           ${var.kubernetes_override_ip} ${trim(var.kubernetes_override_domains, "\"")}
+#           fallthrough
+#     }
+# }
+# EOF
+#     "default.override" = ""
+#   }
+# }
 
-# When kubernetes_cluster_type is kubeadm, then patch the coredns configmap to load the custom coredns config. This is to avoid hairpin NAT issue.
-# Reference: https://github.com/k3s-io/k3s/blob/master/manifests/coredns.yaml#L75
-resource "null_resource" "patch_coredns_configmap" {
-  count = var.kubernetes_cluster_type == "kubeadm" ? 1 : 0
+# # When kubernetes_cluster_type is kubeadm, then patch the coredns configmap to load the custom coredns config. This is to avoid hairpin NAT issue.
+# # Reference: https://github.com/k3s-io/k3s/blob/master/manifests/coredns.yaml#L75
+# resource "null_resource" "patch_coredns_configmap" {
+#   count = var.kubernetes_cluster_type == "kubeadm" ? 1 : 0
 
-  triggers = {
-    kubernetes_cluster_type     = var.kubernetes_cluster_type
-    kubernetes_override_domains = var.kubernetes_override_domains
-    # always_run          = "${timestamp()}"
-  }
+#   triggers = {
+#     kubernetes_cluster_type     = var.kubernetes_cluster_type
+#     kubernetes_override_domains = var.kubernetes_override_domains
+#     # always_run          = "${timestamp()}"
+#   }
 
-  provisioner "local-exec" {
-    command = <<EOT
-      kubectl get configmap coredns -n kube-system -o yaml > /tmp/coredns-configmap.yaml
-      # Only add the override import if it doesn't already exist
-      grep -q "import /etc/coredns/custom/\*.override" /tmp/coredns-configmap.yaml || sed -i '/loadbalance/a \ \ \ \ \ \ \ \ import /etc/coredns/custom/*.override' /tmp/coredns-configmap.yaml
-      # Only add the server import if it doesn't already exist
-      grep -q "import /etc/coredns/custom/\*.server" /tmp/coredns-configmap.yaml || sed -i '/^    }$/a \ \ \ \ import /etc/coredns/custom/*.server'  /tmp/coredns-configmap.yaml
-      kubectl apply -f /tmp/coredns-configmap.yaml
-    EOT
-  }
-}
+#   provisioner "local-exec" {
+#     command = <<EOT
+#       kubectl get configmap coredns -n kube-system -o yaml > /tmp/coredns-configmap.yaml
+#       # Only add the override import if it doesn't already exist
+#       grep -q "import /etc/coredns/custom/\*.override" /tmp/coredns-configmap.yaml || sed -i '/loadbalance/a \ \ \ \ \ \ \ \ import /etc/coredns/custom/*.override' /tmp/coredns-configmap.yaml
+#       # Only add the server import if it doesn't already exist
+#       grep -q "import /etc/coredns/custom/\*.server" /tmp/coredns-configmap.yaml || sed -i '/^    }$/a \ \ \ \ import /etc/coredns/custom/*.server'  /tmp/coredns-configmap.yaml
+#       kubectl apply -f /tmp/coredns-configmap.yaml
+#     EOT
+#   }
+# }
 
-# When kubernetes_cluster_type is kubeadm, then patch the coredns deployment to load the custom coredns configmap. This is to avoid hairpin NAT issue.
-resource "null_resource" "patch_coredns_deployment" {
-  count = var.kubernetes_cluster_type == "kubeadm" ? 1 : 0
+# # When kubernetes_cluster_type is kubeadm, then patch the coredns deployment to load the custom coredns configmap. This is to avoid hairpin NAT issue.
+# resource "null_resource" "patch_coredns_deployment" {
+#   count = var.kubernetes_cluster_type == "kubeadm" ? 1 : 0
 
-  triggers = {
-    kubernetes_cluster_type     = var.kubernetes_cluster_type
-    kubernetes_override_domains = var.kubernetes_override_domains
-    # always_run          = "${timestamp()}"
-  }
+#   triggers = {
+#     kubernetes_cluster_type     = var.kubernetes_cluster_type
+#     kubernetes_override_domains = var.kubernetes_override_domains
+#     # always_run          = "${timestamp()}"
+#   }
 
-  provisioner "local-exec" {
-    command = <<EOT
-      # Check if the volume already exists
-      if ! kubectl get deployment coredns -n kube-system -o jsonpath='{.spec.template.spec.volumes[*].name}' | grep -q "coredns-custom"; then
-        echo "Adding coredns-custom volume..."
-        kubectl patch deployment coredns -n kube-system --type json -p '[{"op": "add", "path": "/spec/template/spec/volumes/-", "value": {"name": "coredns-custom", "configMap": {"name": "coredns-custom", "optional": true}}}]'
-      else
-        echo "coredns-custom volume already exists"
-      fi
+#   provisioner "local-exec" {
+#     command = <<EOT
+#       # Check if the volume already exists
+#       if ! kubectl get deployment coredns -n kube-system -o jsonpath='{.spec.template.spec.volumes[*].name}' | grep -q "coredns-custom"; then
+#         echo "Adding coredns-custom volume..."
+#         kubectl patch deployment coredns -n kube-system --type json -p '[{"op": "add", "path": "/spec/template/spec/volumes/-", "value": {"name": "coredns-custom", "configMap": {"name": "coredns-custom", "optional": true}}}]'
+#       else
+#         echo "coredns-custom volume already exists"
+#       fi
 
-      # Check if the volumeMount already exists
-      if ! kubectl get deployment coredns -n kube-system -o jsonpath='{.spec.template.spec.containers[0].volumeMounts[*].name}' | grep -q "coredns-custom"; then
-        echo "Adding coredns-custom volumeMount..."
-        kubectl patch deployment coredns -n kube-system --type json -p '[{"op": "add", "path": "/spec/template/spec/containers/0/volumeMounts/-", "value": {"name": "coredns-custom", "mountPath": "/etc/coredns/custom", "readOnly": true}}]'
-      else
-        echo "coredns-custom volumeMount already exists"
-      fi
-    EOT
-  }
-}
+#       # Check if the volumeMount already exists
+#       if ! kubectl get deployment coredns -n kube-system -o jsonpath='{.spec.template.spec.containers[0].volumeMounts[*].name}' | grep -q "coredns-custom"; then
+#         echo "Adding coredns-custom volumeMount..."
+#         kubectl patch deployment coredns -n kube-system --type json -p '[{"op": "add", "path": "/spec/template/spec/containers/0/volumeMounts/-", "value": {"name": "coredns-custom", "mountPath": "/etc/coredns/custom", "readOnly": true}}]'
+#       else
+#         echo "coredns-custom volumeMount already exists"
+#       fi
+#     EOT
+#   }
+# }
