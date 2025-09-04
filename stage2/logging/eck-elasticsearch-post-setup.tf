7c6164b3c3c23e59e5c31129d41fdf5ff70e0667
Chris Lee <git@chrislee.kr>
2025-09-04 23:39:49 +1000

    chore: change to octal for readability

--- stage2/logging/eck-elasticsearch-post-setup.tf
+++ stage2/logging/eck-elasticsearch-post-setup.tf
@@ -77,8 +77,9 @@ resource "kubectl_manifest" "elasticsearch_post_setup" {
           volumes = [{
             name = "setup-script"
             configMap = {
-              name        = kubernetes_config_map.elasticsearch_setup_script.metadata[0].name
-              defaultMode = 493 # 0755 in decimal
+              name = kubernetes_config_map.elasticsearch_setup_script.metadata[0].name
+              # defaultMode is optional: mode bits used to set permissions on created files by default. Must be an octal value between 0000 and 0777 or a decimal value between 0 and 511. YAML accepts both octal and decimal values, JSON requires decimal values for mode bits. Defaults to 0644. Directories within the path are not affected by this setting. This might be in conflict with other options that affect the file mode, like fsGroup, and the result can be other mode bits set.
+              defaultMode = "0755"
             }
           }]
         }
