4834bcf6c045812780b0bf3e5889167aedf41c06
Chris Lee <git@chrislee.kr>
2025-09-04 23:56:04 +1000

    revert: elasticsearch_post_setup

--- stage2/logging/eck-elasticsearch-post-setup.tf
+++ stage2/logging/eck-elasticsearch-post-setup.tf
@@ -23,72 +23,71 @@ resource "kubernetes_config_map" "elasticsearch_setup_script" {
 }
 
 # Create ConfigMap for the setup script and templates
-resource "kubectl_manifest" "elasticsearch_post_setup" {
+resource "kubernetes_job" "elasticsearch_post_setup" {
   depends_on = [
     kubernetes_config_map.elasticsearch_setup_script,
     data.kubernetes_resource.elasticsearch,
     random_password.elastic_password
   ]
 
-  yaml_body = yamlencode({
-    apiVersion = "batch/v1"
-    kind       = "Job"
-    metadata = {
-      name      = "elasticsearch-post-setup"
-      namespace = kubernetes_namespace.logging.metadata[0].name
-      # Add elasticsearch version to force job recreation when elasticsearch changes
-      annotations = {
-        "elasticsearch-version" = data.kubernetes_resource.elasticsearch.object.spec.version
+  metadata {
+    name      = "elasticsearch-post-setup"
+    namespace = kubernetes_namespace.logging.metadata[0].name
+  }
+
+  spec {
+
+    template {
+      metadata {
+        # Add elasticsearch version to force job recreation when elasticsearch changes
+        annotations = {
+          "elasticsearch-version" = data.kubernetes_resource.elasticsearch.object.spec.version
+        }
       }
-    }
-    spec = {
-      backoffLimit = 1
-      # Add TTL to automatically clean up completed jobs
-      ttlSecondsAfterFinished = 300
-      template = {
-        metadata = {
-          # Add elasticsearch version to force job recreation when elasticsearch changes
-          annotations = {
-            "elasticsearch-version" = data.kubernetes_resource.elasticsearch.object.spec.version
+
+      spec {
+        container {
+          name  = "elasticsearch-post-setup"
+          image = "alpine:3.18"
+
+          env {
+            name  = "ELASTIC_PASSWORD"
+            value = random_password.elastic_password.result
+          }
+
+          env {
+            name  = "ELASTICSEARCH_URL"
+            value = "http://elasticsearch-es-http.${kubernetes_namespace.logging.metadata[0].name}.svc:9200"
+          }
+
+          volume_mount {
+            name       = "setup-script"
+            mount_path = "/scripts"
+          }
+
+          command = ["/bin/sh"]
+          args    = ["/scripts/elasticsearch-post-setup.sh"]
+        }
+
+        volume {
+          name = "setup-script"
+          config_map {
+            name         = kubernetes_config_map.elasticsearch_setup_script.metadata[0].name
+            default_mode = "0755"
           }
         }
-        spec = {
-          restartPolicy = "Never"
-          containers = [{
-            name  = "elasticsearch-post-setup"
-            image = "alpine:3.18"
-            env = [
-              {
-                name  = "ELASTIC_PASSWORD"
-                value = random_password.elastic_password.result
-              },
-              {
-                name  = "ELASTICSEARCH_URL"
-                value = "http://elasticsearch-es-http.${kubernetes_namespace.logging.metadata[0].name}.svc:9200"
-              }
-            ]
-            volumeMounts = [{
-              name      = "setup-script"
-              mountPath = "/scripts"
-            }]
-            command = ["/bin/sh"]
-            args    = ["/scripts/elasticsearch-post-setup.sh"]
-          }]
-          volumes = [{
-            name = "setup-script"
-            configMap = {
-              name = kubernetes_config_map.elasticsearch_setup_script.metadata[0].name
-              # defaultMode is optional: mode bits used to set permissions on created files by default. Must be an octal value between 0000 and 0777 or a decimal value between 0 and 511. YAML accepts both octal and decimal values, JSON requires decimal values for mode bits. Defaults to 0644. Directories within the path are not affected by this setting. This might be in conflict with other options that affect the file mode, like fsGroup, and the result can be other mode bits set.
-              defaultMode = "0755"
-            }
-          }]
-        }
+
+        restart_policy = "Never"
       }
     }
-  })
+
+    backoff_limit = 1
+  }
 
   timeouts {
     create = "3m"
+    update = "3m"
   }
 
+  wait_for_completion = true
 }
--- stage2/logging/eck-stack.tf
+++ stage2/logging/eck-stack.tf
@@ -222,7 +222,7 @@ resource "kubernetes_manifest" "filebeat_cluster_role_binding" {
 resource "kubectl_manifest" "filebeat" {
   depends_on = [
     kubectl_manifest.elasticsearch,
-    kubectl_manifest.elasticsearch_post_setup,
+    kubernetes_job.elasticsearch_post_setup,
     kubernetes_manifest.filebeat_cluster_role,
     kubernetes_manifest.filebeat_service_account,
     kubernetes_manifest.filebeat_cluster_role_binding,
